86a08988b7db7e39e1a6545f6b83066eb93da776
phamtanhung <phamtanhung@gmail.com>
2014-03-14 00:29:52 +0700

    done schedule metrics

--- sources/js/todo.js
+++ sources/js/todo.js
@@ -6,19 +6,20 @@ angular.module('todo', [])
 	.controller('TodoCtrl', function($scope){
 		$scope.todos = [
 			{text:'learn bootstrap', status:'done', due: '1/18/2013', cat: 'workplace'},
-			{text:'learn foudation', status:'done', due: '2/22/2013', cat: 'workplace'},
 			{text:'learn angular', status:'unplanned', due: '3/22/2014', cat: 'workplace'},
-			{text:'build Todo sample', status:'in_progress', due: '3/16/2014', cat: 'workplace'},
+			{text:'build Todo sample', status:'in_progress', due: '3/18/2014', cat: 'workplace'},
+			{text:'learn foudation 2', status:'in_progress', due: '1/15/2014', cat: 'workplace'},
+			{text:'learn angular 3', status:'planned', due: '3/11/2014', cat: 'workplace'},
+			{text:'build Todo sample 3', status:'done', due: '1/16/2014', cat: 'workplace'},
+
+			{text:'learn foudation', status:'planned', due: '3/22/2013', cat: 'homeplace'},
 			{text:'build an angular app 2', status:'planned', due: '4/22/2014', cat: 'homeplace'},
 			{text:'learn bootstrap 2', status:'done', due: '1/18/2013', cat: 'homeplace'},
-			{text:'learn foudation 2', status:'done', due: '2/22/2013', cat: 'homeplace'},
-			{text:'learn angular 2', status:'unplanned', due: '3/22/2014', cat: 'homeplace'},
-			{text:'build Todo sample 2', status:'in_progress', due: '3/16/2014', cat: 'homeplace'},
-			{text:'build an angular app 2', status:'planned', due: '4/22/2014', cat: 'homeplace'},
+			{text:'learn angular 2', status:'unplanned', due: '1/22/2014', cat: 'homeplace'},
+			{text:'build Todo sample 2', status:'in_progress', due: '1/16/2014', cat: 'homeplace'},
+			{text:'build an angular app 2', status:'planned', due: '3/17/2014', cat: 'homeplace'},
 			{text:'learn bootstrap 3', status:'done', due: '1/18/2013', cat: 'homeplace'},
-			{text:'learn foudation 3', status:'in_progress', due: '2/22/2013', cat: 'homeplace'},
-			{text:'learn angular 3', status:'planned', due: '3/22/2014', cat: 'workplace'},
-			{text:'build Todo sample 3', status:'done', due: '3/16/2014', cat: 'homeplace'},
+			{text:'learn foudation 3', status:'in_progress', due: '3/15/2013', cat: 'homeplace'},
 			{text:'build an angular app 3', status:'done', due: '4/22/2014', cat: 'homeplace'}
 		];
 		$scope.todosCurrent = $scope.todos;
@@ -55,15 +56,22 @@ angular.module('todo', [])
 		  if (todo.cat == type) {
 		  	$scope.todosCurrent.push(todo);
 		  }
+		  $('.nav-cat li').removeClass('active');
+		  $('.nav-cat li[data-type="'+ type +'"]').addClass('active');
 		});
+		//console.log($scope.todosCurrent.length);
 	  };
-
+	  $scope.filter('workplace');
 
 	  /*Schedule Metrics*/
 	  $scope.metrics = function(type) {
-		var count = 0;
+		var count_task_done = 0;
+		var count_task_deadline = 0;
+		var count_task_overdue = 0;
+
+		var now =  new Date();
 		angular.forEach($scope.todosCurrent, function(todo) {
-		  count += todo.status == 'done' ? 0 : 1;
+
 		});
 		return (count/$scope.todosCurrent.length) * 100;
 	  };
@@ -71,25 +79,43 @@ angular.module('todo', [])
 
 	  $scope.metricsDone = function() {
 		var count = 0;
+		var now =  new Date();
 		angular.forEach($scope.todosCurrent, function(todo) {
-		  count += todo.status == 'unplanned' ? 0 : 1;
+		  count += todo.status == 'done' ? 1 : 0;
 		});
+
+		// console.log('metricsDone');	
+		// console.log(count/$scope.todosCurrent.length);
 		return (count/$scope.todosCurrent.length) * 100;
 	  };
 
 	  $scope.metricsDeadline = function() {
 		var count = 0;
+		
 		angular.forEach($scope.todosCurrent, function(todo) {
-		  count += todo.status == 'in_progress' ? 0 : 1;
-		});
-
-		//return count;
+			var now =  new Date();
+			var millisecondsPerDay = 24 * 60 * 60 * 1000;
+			var nextDay = now.getTime() + millisecondsPerDay*7;
+			var arrDate = todo.due.split('/');
+			var dueDay = new Date(arrDate[2], arrDate[0]-1, arrDate[1], now.getHours(), now.getMinutes(), now.getSeconds());
+			if(dueDay.getTime() <= nextDay) {				
+		  		count += todo.status != 'done' ? 1 : 0;
+			}
+		});	
+		// console.log('metricsDeadline');	
+		// console.log(count + " - "+ $scope.todosCurrent.length);	
 		return (count/$scope.todosCurrent.length) * 100;
 	  };
+
 	  $scope.metricsOverdue = function() {
 		var count = 0;
 		angular.forEach($scope.todosCurrent, function(todo) {
-		  count += todo.status == 'in_progress' ? 0 : 1;
+		  	var now =  new Date();
+			var arrDate = todo.due.split('/');
+			var dueDay = new Date(arrDate[2], arrDate[0]-1, arrDate[1], now.getHours(), now.getMinutes(), now.getSeconds());
+			if(dueDay.getTime() <= now) {				
+		  		count += todo.status != 'done' ? 1 : 0;
+			}  
 		});
 
 		//return count;
@@ -106,6 +132,7 @@ TODO.initElements = function(){
 	});
 
 	/*Checkbox All handel*/
+	/*
 	$('.chk-list').each(function(index, list){
 		list = $(list);
 		list.items = list.find('.chk-item');
@@ -131,6 +158,26 @@ TODO.initElements = function(){
 		});
 		list.update();
 	});
+	*/
+	
+	$('.chk-all').on('change', function(){
+		var chkName = $(this).attr('name');
+		if($(this).is(':checked')){
+			$('.chk-item[name="' + chkName +'"]').prop('checked', true);
+		} else {
+			$('.chk-item[name="' + chkName +'"]').prop('checked', false);
+		}
+	});
+
+	$('.chk-item').on('change', function(){
+		var chkName = $(this).attr('name');
+		if($('.chk-item[name="' + chkName +'"]').length == $('.chk-item[name="' + chkName +'"]:checked').length){
+			$('.chk-all[name="' + chkName +'"]').prop('checked', true);
+		} else {
+			$('.chk-all[name="' + chkName +'"]').prop('checked', false);
+		}
+	});
+
 
 }
 $(document).ready(function(){
